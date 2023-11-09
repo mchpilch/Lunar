68d5ce8d89ccef7ac7447eb242133779c35fc4af
mchpilch <mchpilch@gmail.com>
2023-11-09 19:32:05 +0100

    Camera follows Earth after onClick

--- src/MainContainer.jsx
+++ src/MainContainer.jsx
@@ -1,11 +1,7 @@
 import { useHelper } from "@react-three/drei";
-import React, { useRef, useMemo, useCallback } from "react";
-import * as THREE from 'three'
-import { useFrame } from "@react-three/fiber";
+import React from "react";
 import AnimatedStars from "./AnimatedStars";
 import Earth from "./scenes/earth/Earth";
-import Moon from "./scenes/earth/Moon";
-import ISS from "./scenes/earth/ISS";
 import Sun from "./scenes/sun/Sun";
 
 
@@ -14,20 +10,6 @@ const MainContainer = () => {
     // const directionalLightRefTwo = useRef();
     //useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1, 'hotpink');
     //useHelper(directionalLightRefTwo, THREE.DirectionalLightHelper, 1, 'hotpink');
-    const clockRef = useRef(new THREE.Clock())
-    const groupRef = useRef();
-
-    const Amp = 8;
-    const AngularSpeed = 0.1025;
-    const updateGroupPosition = useCallback(() => {
-        const x = Math.sin(clockRef.current.getElapsedTime() * AngularSpeed) * Amp;
-        const z = Math.cos(clockRef.current.getElapsedTime() * AngularSpeed) * Amp;
-        groupRef.current.position.set(x, 0, z);
-    }, [])
-    useFrame(() => {
-        updateGroupPosition();
-    })
-
     return (<>
         <AnimatedStars />
         <axesHelper args={[50]} />
@@ -35,11 +17,7 @@ const MainContainer = () => {
         {/* <directionalLight ref={directionalLightRefTwo} position={[0, 0, -10]} castShadow={true} intensity={4}/> */}
         {/* <ambientLight intensity={4} /> */}
         <Sun />
-        <group ref={groupRef}>
-            <Earth displacementScale={0.075} />
-            <ISS />
-            <Moon />
-        </group>
+        <Earth displacementScale={0.075} />
     </>);
 }
 
--- src/scenes/earth/Earth.jsx
+++ src/scenes/earth/Earth.jsx
@@ -1,6 +1,9 @@
 import { useTexture } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
-import React, { useRef, useEffect, useState } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import React, { useRef, useEffect, useState, useCallback } from "react";
+import * as THREE from 'three';
+import Moon from "./Moon";
+import ISS from "./ISS";
 
 const Earth = React.memo(({ displacementScale }) => {
     const [earthTexture, earthNormalMap, earthSpecularMap, earthDisplacementMap, earthEmissiveMap] = useTexture([
@@ -11,34 +14,68 @@ const Earth = React.memo(({ displacementScale }) => {
         '/assets/earth_night.jpg',
     ]);
 
+     const {camera} = useThree();
+    const clockRef = useRef(new THREE.Clock());
     const earthRef = useRef();
+
+    const Amp = 8;
+    const AngularSpeed = 0.1025;
+    const updateEarthPosition = useCallback(() => {
+        const x = Math.sin(clockRef.current.getElapsedTime() * AngularSpeed) * Amp;
+        const z = Math.cos(clockRef.current.getElapsedTime() * AngularSpeed) * Amp;
+        earthRef.current.position.set(x, 0, z);
+    }, []);
+
     useFrame(() => {
         earthRef.current.rotation.y += 0.022;
-    })
+        updateEarthPosition();
+        const earthPositionRef = earthRef.current.position;
+        console.log('it happens');
+        if (followingEarth) {
+            camera.lookAt(earthPositionRef);
+            console.log('it happens');
+        }
+    });
 
     const [hovered, hover] = useState(false);
+    const [followingEarth, setFollowingEarth] = useState(false);
+
+    const toggleFollowingEarth = () => {
+        setFollowingEarth((prevFollowingEarth) => !prevFollowingEarth)
+    }
+
     useEffect(() => {
         document.body.style.cursor = hovered ? 'pointer' : 'auto';
     }, [hovered])
 
-    return <mesh ref={earthRef} castShadow
-        receiveShadow onPointerOver={()=>hover(true)}
-        onPointerOut={()=>hover(false)}
-        >
-        <sphereGeometry args={[1, 32, 32]}></sphereGeometry>
-        <meshPhongMaterial
-            map={earthTexture}
-            normalMap={earthNormalMap}
-            shininess={2500}
-            specularMap={earthSpecularMap}
-            displacementMap={earthDisplacementMap}
-            displacementScale={displacementScale}
-            emissiveMap={earthEmissiveMap}
-            emissiveIntensity={hovered ? 8 : 1.75}
-            emissive={0xffff00}
-        // /* radius, X -axis, Y - axis *   color='blue'/
-        />
-    </mesh>
+    return (
+        <group ref={earthRef}>
+            <mesh
+                ref={earthRef}
+                castShadow
+                receiveShadow
+                onPointerOver={() => hover(true)}
+                onPointerOut={() => hover(false)}
+                onClick={toggleFollowingEarth}
+            >
+                <sphereGeometry args={[1, 32, 32]}></sphereGeometry>
+                <meshPhongMaterial
+                    map={earthTexture}
+                    normalMap={earthNormalMap}
+                    shininess={2500}
+                    specularMap={earthSpecularMap}
+                    displacementMap={earthDisplacementMap}
+                    displacementScale={displacementScale}
+                    emissiveMap={earthEmissiveMap}
+                    emissiveIntensity={hovered ? 9 : 4}
+                    emissive={0xffff00}
+                // /* radius, X -axis, Y - axis *   color='blue'/
+                />
+                <ISS />
+                <Moon />
+            </mesh>
+        </group>
+    )
 })
 
 export default Earth;
